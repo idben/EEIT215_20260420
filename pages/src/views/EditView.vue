8dfc765c1a634b1e47ea4680828e153cf47f80a4
Ben Chen <idben.chen@gmail.com>
2026-04-20 10:53:08 +0800

    導航守衛03 - 2

--- pages/src/views/EditView.vue
+++ pages/src/views/EditView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from 'vue';
 
+import { onBeforeRouteLeave } from 'vue-router';
+
 const formData = ref("3344")
 const isSaved = ref(true)
 
@@ -9,13 +11,23 @@ const handleInput = (event) => {
     console.log(formData.value);
     isSaved.value = false;
 }
+
+const handleSave = () => {
+    isSaved.value = true;
+}
+
+onBeforeRouteLeave(() => {
+    if (isSaved.value) return;
+    const answer = confirm("有未儲存的變更，是否要離開??")
+    if (!answer) return false;
+})
 </script>
 <template>
     <h1>編輯頁面</h1>
     <input :value="formData" @input="handleInput" type="text" class="form-control" placeholder="請輸入內容">
     <div v-if="!isSaved" class="text-danger">尚未儲存...</div>
     <div class="d-flex mt-3">
-        <button class="btn btn-primary ms-auto">儲存</button>
+        <button @click="handleSave" class="btn btn-primary ms-auto">儲存</button>
     </div>
 
 </template>
