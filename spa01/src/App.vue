82777953a69e54df1a3f400159a4656eab812503
Ben Chen <idben.chen@gmail.com>
2026-04-20 16:06:06 +0800

    bootstrap 的引入

--- spa01/src/App.vue
+++ spa01/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { RouterView } from 'vue-router';
+import NavBar from './components/NavBar.vue';
 </script>
 <template>
+  <NavBar />
   <div class="container py3">
     <RouterView />
   </div>
--- spa01/src/main.js
+++ spa01/src/main.js
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.bundle.js"
 import './assets/main.css'
 
 import { createApp } from 'vue'
