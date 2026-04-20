404f24c3c66294c669affa244dc61698b4e58f8c
Ben Chen <idben.chen@gmail.com>
2026-04-20 09:45:24 +0800

    動態路由

---
+++ pages/src/views/ArticleView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+</script>
+<template>
+    <h1>文章詳情</h1>
+    <h2>文章ID(動態路由): {{ route.params.id }}</h2>
+    <h3>目前路徑: {{ route.path }}</h3>
+</template>
+<style scoped></style>
