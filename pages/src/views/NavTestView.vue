fb572e6d607a9e82abeb9910d1a24ab4c206c075
Ben Chen <idben.chen@gmail.com>
2026-04-20 11:25:05 +0800

    程式導航測試 -2

--- pages/src/views/NavTestView.vue
+++ pages/src/views/NavTestView.vue
@@ -17,7 +17,7 @@ const goBack = () => {
         <h2>程式化導航測試</h2>
         <button @click="goAbout" class="btn btn-primary">用 JS 跳到關於頁</button>
         <button @click="goBack" class="btn btn-danger">返回上一頁</button>
-        <button class="btn btn-info">跳到文章 99</button>
+        <button @click="router.push({ name: 'article', params: { id: 99 } })" class="btn btn-info">跳到文章 99</button>
     </div>
 </template>
 
