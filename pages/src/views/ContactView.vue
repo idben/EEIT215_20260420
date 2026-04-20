ffac6375e6f159f95c0ec2e32429864ef11ae9b9
Ben Chen <idben.chen@gmail.com>
2026-04-20 09:27:39 +0800

    pages init

---
+++ pages/src/views/ContactView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+    <div>
+        <h2>聯絡我們</h2>
+        <p>這是聯絡我們頁面</p>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ pages/src/views/NewsView.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+<template>
+    <div>
+        <h2>最新消息</h2>
+        <p>這是最新消息頁面</p>
+    </div>
+</template>
+
+<style scoped></style>
