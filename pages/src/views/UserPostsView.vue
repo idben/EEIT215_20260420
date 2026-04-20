a49400b6e477c80a17686d6bea693390faea1f76
Ben Chen <idben.chen@gmail.com>
2026-04-20 11:33:45 +0800

    巢狀路由 -1

---
+++ pages/src/views/UserPostsView.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+<template>
+    <div class="card">
+        <div class="card-header">
+            <h3>我的文章</h3>
+        </div>
+        <div class="card-body">
+            <p>這是我的文章頁面</p>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ pages/src/views/UserSettingsView.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+<template>
+    <div class="card">
+        <div class="card-header">
+            <h3>設定</h3>
+        </div>
+        <div class="card-body">
+            <p>這是設定頁面</p>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
