9d4b3e115230b0a3c7046b3f7552abaa1294a02c
Ben Chen <idben.chen@gmail.com>
2026-04-20 13:54:24 +0800

    loadingOverLay

---
+++ pages/src/components/LoadingOverlay.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+<template>
+    <div class="loading">
+        Loading...
+    </div>
+</template>
+<style scoped>
+.loading {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #14c2e9;
+    color: #fff;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 999;
+}
+</style>
