98bc9a1cf6e09fe91c0cb511c01ada31acb14e97
Ben Chen <idben.chen@gmail.com>
2026-04-20 09:32:22 +0800

    RouterLink 使用

--- pages/src/App.vue
+++ pages/src/App.vue
@@ -13,6 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/news">最新消息</RouterLink>
+        <RouterLink to="/contact">聯絡我們</RouterLink>
       </nav>
     </div>
   </header>
